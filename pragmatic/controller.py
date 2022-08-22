41583b5426b0b9e565e77e3f1df512209c4e0c15
Zachary Hampton <69336300+ZacharyHampton@users.noreply.github.com>
2022-08-21 21:18:56 -0700

    - rollback controller.py

--- pragmatic/controller.py
+++ pragmatic/controller.py
@@ -3,28 +3,28 @@ import time
 import asyncio
 import xmltodict
 import pragmatic.exceptions
-
-websocket.enableTrace(True)
+from pragmatic.actions import PragmaticActions
 
 
 class PragmaticController:
     def __init__(self, sessionId: str, tableId: str, handler=None):
-        self.sessionId = sessionId
+        self._sessionId = sessionId
         self.tableId = tableId
-        self.handler = handler
-        self.ws: websocket.WebSocket = websocket.WebSocket()
+        self._handler = handler
+        self._ws: websocket.WebSocket = websocket.WebSocket()
         self._event_loop = asyncio.get_event_loop()
+        self.actions = PragmaticActions(self._ws, self.tableId)
 
     async def _handle_data(self):
-        while self.ws.connected:
+        while self._ws.connected:
             try:
-                data = self.ws.recv()
+                data = self._ws.recv()
             except websocket.WebSocketConnectionClosedException:
                 print('Disconnected, reconnecting...')
                 await self.connect(reconnect=True)
                 return await self._handle_data()
 
-            if self.handler:
+            if self._handler:
                 if data:
                     if data == "Connection Exception":
                         raise pragmatic.exceptions.PragmaticSessionInvalid("Connection Exception")
@@ -32,31 +32,31 @@ class PragmaticController:
                     if "duplicated_connection" in data:
                         raise pragmatic.exceptions.PragmaticDuplicateSession("Two sessions are active.")
 
-                    await self.handler(self, xmltodict.parse(data))
+                    await self._handler(self, xmltodict.parse(data))
 
     async def _ping(self):
         while True:
-            while self.ws.connected:
+            while self._ws.connected:
                 time.sleep(10)
-                self.ws.send('<ping time={}></ping>'.format(int(time.time())))
+                self._ws.send('<ping time={}></ping>'.format(int(time.time())))
 
     async def connect(self, reconnect=False):
         if reconnect is False:
-            self.ws.connect(
-                'wss://gs7.pragmaticplaylive.net/game?JSESSIONID={}&tableId={}'.format(self.sessionId, self.tableId),
+            self._ws.connect(
+                'wss://gs7.pragmaticplaylive.net/game?JSESSIONID={}&tableId={}'.format(self._sessionId, self.tableId),
                 origin='https://client.pragmaticplaylive.net')
 
-            if self.ws.connected:
+            if self._ws.connected:
                 print('Connected.')
 
             self._event_loop.create_task(self._handle_data())
             self._event_loop.create_task(self._ping())
             self._event_loop.run_forever()
         else:
-            self.ws.connect(
-                'wss://gs7.pragmaticplaylive.net/game?JSESSIONID={}&tableId={}&reconnect=true'.format(self.sessionId,
+            self._ws.connect(
+                'wss://gs7.pragmaticplaylive.net/game?JSESSIONID={}&tableId={}&reconnect=true'.format(self._sessionId,
                                                                                                       self.tableId),
                 origin='https://client.pragmaticplaylive.net')
 
-        if self.ws.connected:
+        if self._ws.connected:
             print('Connected.')
