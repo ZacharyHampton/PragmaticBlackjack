b9e0cddd4e87f16a5c19c30fba38b0311374abe7
zacharyhampton <zhampton7131@dysartstudents.org>
2022-08-31 12:15:58 -0700

    - type init

--- examples/test/main.py
+++ examples/test/main.py
@@ -2,31 +2,40 @@ from pragmatic.controller import PragmaticController
 import asyncio
 import os
 from dotenv import load_dotenv
-import websocket
+import halves
+from classes import Table
 
 load_dotenv()
 SESSIONID = os.getenv('SESSIONID')
 TABLEID = os.getenv('TABLEID')
-CARDSPLAYED = 0
+
+GameTable = Table()
+CardCounter = halves.GameState(decks=7, bank_roll=100.00)
+AllowGame = False
+SeatNumber = 1
+
 
 # <betsopen> = open for bets
 # <seat> = describes the seat (who is on it) (check for open seats)
 #: <card> = describes dealt cards
-
+#: split case on decision: <decisioninc seat="2" score="4" game="1850564704" cansplit="true" dealerscore="10" id="by7mco374logcksp9vcsjixbybnew2fg" time="14" candouble="true" preautostand="false" userid="ppc1661951196580" hand="0" seq="710"></decisioninc>
+#: <pre_decisioninc seat="2" score="4" game="1850564704" cansplit="true" candouble="true" hand="0" seq="664"></pre_decisioninc>
+#: dealer seat is -1
 
 async def handler(game: PragmaticController, data: dict):
-    global CARDSPLAYED
-    if data.get('card'):
-        if data['card']['@initial'] == 'false':
-            CARDSPLAYED += 1
-            print("Cards played:", CARDSPLAYED, data)
-    # print(data)
+    if not AllowGame:
+        pass
+
 
 
 async def main():
     game = PragmaticController(SESSIONID, TABLEID, handler=handler)
     await game.connect()
-    input('Press Enter to exit...')
+    while True:
+        input("Press enter on every shuffle.")
+        CardCounter.shuffle()
+        global AllowGame
+        AllowGame = True
 
 
 if __name__ == '__main__':
