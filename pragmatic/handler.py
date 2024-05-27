2b8e6f5dbe1412efc4929485fd764f2fd748632c
Zachary Hampton <69336300+ZacharyHampton@users.noreply.github.com>
2024-05-26 22:59:35 -0700

    - basic outline

--- pragmatic/handler.py
+++ pragmatic/handler.py
@@ -1,3 +1,14 @@
+from dataclasses import dataclass
+from typing import Callable
+from .event import Event
+
+
+@dataclass
+class Handle:
+    function: Callable[[Event | str], None]
+    raw: bool = False
+
+
 class HandlerBase:
     def __init__(self, *args, **kwargs):
         pass
--- pragmatic/table.py
+++ pragmatic/table.py
@@ -1,6 +1,6 @@
 from typing import Callable
 from .event import Event
-from .handler import HandlerBase
+from .handler import HandlerBase, Handle
 
 
 class Table:
@@ -9,16 +9,35 @@ class Table:
             table_id: str,
             session_id: str,
             handler: HandlerBase = None,
-            handles: list = None,
+            handles: dict[Event, Handle] = None,
     ):
         self.table_id = table_id
         self.session_id = session_id
 
-    def register(self, handler: HandlerBase):
-        pass
+        self.handler = handler
+        self.handles = handles
 
-    def register_handle(self, event: Event, function: Callable[[Event], None]):
-        pass
+    def register(self, handler: HandlerBase) -> None:
+        """
+        Register a handler class to the table
+
+        :param handler: Handler class to register
+        :return: None
+        """
+
+        self.handler = handler
+
+    def register_handle(self, event: Event, function: Callable[[Event | str], None], raw: bool = False) -> None:
+        """
+        Register an event to a function
+
+        :param event: Event type to register
+        :param function: Function to call when event is triggered
+        :param raw: If True, the function will be called with the raw event data
+        :return: None
+        """
+
+        self.handles[event] = Handle(function, raw)
 
     def connect(self):
         pass
